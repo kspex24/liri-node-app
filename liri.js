07bc59904009eff98aa8a8cdbe5f14418ad773c5
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-04-11 22:46:51 -0400

    inquirer added

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 
+//Define variables and require packages, files
 require("dotenv").config();
 
 var fs = require("fs");
@@ -9,174 +10,182 @@ var colors = require('colors');
 
 var inquirer = require('inquirer');
 
-var command = process.argv[2]
-
-var value1 = process.argv[3]
-
 var Twitter = require('twitter');
 
 var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js")
 
-var noMovieQueryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+// Prompt the user to choose a command.
+inquirer.prompt([
 
-var movieQueryUrl = "http://www.omdbapi.com/?t=" + value1 + "&y=&plot=short&apikey=trilogy";
+    {
+        type: "list",
+        message: "Greetings! Please choose a command.",
+        choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "choice"
+    }
 
-// inquirer
-//   .prompt([
-//     // List of commands to choose from
-//     {
-//       type: "list",
-//       message: "Greetings! Please choose a command.",
-//       choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
-//       name: "commandChoice"
-//     }]
-//     .then(function(inquirerResponse) {
-//          var command = inquirerResponse.commandChoice;
+    // After the prompt, store the user's response in a variable called location.
+]).then(function (Response) {
+    var command = Response.choice
 
-//       })
+    // Write switch case to take in user input and respond accordingly:
 
-//     inquirer
-//   .prompt([
-//     // Here we create a basic text prompt.
-//     {
-//       type: "input",
-//       message: "Please enter a search choice:",
-//       name: "value1"
-//     }
-// ])
-//     .then(function(inquirerResponse) {
-//     
-//     });
+    switch (command) {
 
-// Write switch case to take in user input and respond accordingly:
+        case "my-tweets":
+            //call to twitter for last 20 tweets, display in terminal and append search data to log.txt
 
-switch (command) {
-
-    case "my-tweets":
-        //call to twitter for last 20 tweets, display in terminal and append search data to log.txt
-
-        var client = new Twitter(keys.twitter);
-        var params = { screen_name: 'kspex24' };
-        client.get('statuses/user_timeline', params, function (error, tweet, response) {
-            console.log("Tweets from kspex24".cyan.bold);
-            console.log("===============================================================================".cyan.bold)
-
-            for (i = 0; i < 20; i++) {
-                console.log("Created: " + tweet[i].created_at)
-                console.log("Tweet: " + tweet[i].text)
+            var client = new Twitter(keys.twitter);
+            var params = { screen_name: 'kspex24' };
+            client.get('statuses/user_timeline', params, function (error, tweet, response) {
+                console.log("Tweets from kspex24".cyan.bold);
                 console.log("===============================================================================".cyan.bold)
 
+                for (i = 0; i < 20; i++) {
+                    console.log("Created: " + tweet[i].created_at)
+                    console.log("Tweet: " + tweet[i].text)
+                    console.log("===============================================================================".cyan.bold)
+
+                    var logTxt =
+                        "\nDate: " + tweet[i].created_at + " Tweet: " + tweet[i].text
+
+                    fs.appendFile("log.txt", logTxt, function (err) {
+                        if (err) throw err;
+                    });
+                }
                 var logTxt =
-                    "\nDate: " + tweet[i].created_at + " Tweet: " + tweet[i].text
+                    "\n========end========"
 
                 fs.appendFile("log.txt", logTxt, function (err) {
                     if (err) throw err;
                 });
-            }
-            var logTxt =
-                "\n========end========"
-
-            fs.appendFile("log.txt", logTxt, function (err) {
-                if (err) throw err;
+                if (error) {
+                    console.log("Error occurred.")
+                }
             });
-            if (error) {
-                console.log("Error occurred.")
-            }
-        });
 
-        break;
+            break;
 
-    case "spotify-this-song":
-        //call to spotify for song info, display in terminal and append search data to log.txt
+        case "spotify-this-song":
 
-        var spotify = new Spotify(keys.spotify);
+            //Inquirer prompt to see if user has specific song to search:
 
-        //request when song NOT specified
-
-        if (value1 === undefined) {
-            spotify.search({ type: 'track', query: "The Sign" && "Ace of Base" }, function (err, data) {
-
-                if (err) {
-                    return console.log('Error occurred: ' + err);
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter a specific song or just press Enter for a random song search",
+                    name: "song"
                 }
-                printSongInfo(data);
 
+                // After the prompt, store the user's response in a variable called location.
+            ]).then(function (Response) {
+                var value1 = Response.song
+
+                //call to spotify for song info, execute function to display in terminal and append search data to log.txt
+
+                var spotify = new Spotify(keys.spotify);
+
+                //request when song NOT specified
+
+                if (value1 === "") {
+                    spotify.search({ type: 'track', query: "The Sign" && "Ace of Base" }, function (err, data) {
+
+                        if (err) {
+                            return console.log('Error occurred: ' + err);
+                        }
+                        printSongInfo(data);
+
+                    })
+                }
+
+                //request when song IS specified, execute function to display in terminal and append search data to log.txt
+
+                else {
+
+                    spotify.search({ type: 'track', query: value1 }, function (err, data) {
+                        if (err) {
+                            return console.log('Error occurred: ' + err);
+                        }
+                        printSongInfo(data);
+                    })
+                };
             })
-        }
+            break;
 
-        //request when song IS specified, display in terminal and append search data to log.txt
+        case "movie-this":
 
-        else {
+            //Inquirer prompt to see if user has specific movie to search:
 
-            spotify.search({ type: 'track', query: value1 }, function (err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter a specific movie or just press Enter for a random movie search",
+                    name: "movie"
                 }
-                printSongInfo(data);
-            })
-        };
 
-        break;
+                // After the prompt, store the user's response in a variable called value1.
+            ]).then(function (Response) {
+                var value1 = Response.movie
 
-    case "movie-this":
-        //call to OMDB for movie info
-        //this request for when a specific movie is NOT entered, display in terminal and append search data to log.txt
 
-        if (value1 === undefined) {
-            request(noMovieQueryUrl, function (error, response, body) {
+                //call to OMDB for movie info
+                //this request for when a specific movie is NOT entered, display in terminal and append search data to log.txt
 
-                // If the request is successful (i.e. if the response status code is 200)
-                if (!error && response.statusCode === 200) {
+                if (value1 === "") {
+                    request("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+                        // If the request is successful (i.e. if the response status code is 200)
+                        if (!error && response.statusCode === 200) {
+                        }
+                        printMovieInfo(body);
+                    })
                 }
-                printMovieInfo(body);
-            })
-        }
-        //this request for when a specific movie IS entered, display in terminal and append search data to log.txt
-        else {
+                //this request for when a specific movie IS entered, display in terminal and append search data to log.txt
+                else {
 
-            request(movieQueryUrl, function (error, response, body) {
-                // If the request is successful (i.e. if the response status code is 200)
+                    request("http://www.omdbapi.com/?t=" + value1 + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+                        // If the request is successful (i.e. if the response status code is 200)
 
-                if (!error && response.statusCode === 200) {
+                        if (!error && response.statusCode === 200) {
+                        }
+                        printMovieInfo(body);
+                    });
                 }
-                printMovieInfo(body);
             });
-        }
-        break;
+            break;
 
-    case "do-what-it-says":
+        case "do-what-it-says":
 
-        // pull info from random.txt
-        fs.readFile("random.txt", "utf8", function (error, data) {
+            // pull info from random.txt
+            fs.readFile("random.txt", "utf8", function (error, data) {
 
-            // If the code experiences any errors it will log the error to the console.
-            if (error) {
-                return console.log(error);
-            }
-
-            // Split the data in random.txt by commas (to make it more readable)
-            var dataArr = data.split(",");
-
-            command = dataArr[0]
-            value1 = dataArr[1]
-
-            var spotify = new Spotify(keys.spotify);
-
-            spotify.search({ type: 'track', query: value1 }, function (err, data) {
-
-                if (err) {
-                    return console.log('Error occurred: ' + err);
+                // If the code experiences any errors it will log the error to the console.
+                if (error) {
+                    return console.log(error);
                 }
-                printSongInfo(data);
-            });
-        });
 
-        break;
-    default:
-}
+                // Split the data in random.txt by commas (to make it more readable)
+                var dataArr = data.split(",");
+
+                value1 = dataArr[1]
+
+                var spotify = new Spotify(keys.spotify);
+
+                spotify.search({ type: 'track', query: value1 }, function (err, data) {
+
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+                    printSongInfo(data);
+                });
+            });
+
+            break;
+        default:
+    }
+});
 
 //Print and log functions
 function printMovieInfo(body) {
@@ -193,13 +202,13 @@ function printMovieInfo(body) {
     console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
     console.log("----------------------------------------------------------------------------".magenta.bold);
 
-        var logTxt =
-                "\n====New Entry======" + "\nTitle: " + JSON.parse(body).Title + " Year: " + JSON.parse(body).Year + " IMDB Rating: " + JSON.parse(body).imdbRating + " Country: " + JSON.parse(body).Country +
-                " Language: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors + " Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n====End======"
+    var logTxt =
+        "\n====New Entry======" + "\nTitle: " + JSON.parse(body).Title + " Year: " + JSON.parse(body).Year + " IMDB Rating: " + JSON.parse(body).imdbRating + " Country: " + JSON.parse(body).Country +
+        " Language: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors + " Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n====End======"
 
-        fs.appendFile("log.txt", logTxt, function (err) {
-                if (err) throw err;
-        });
+    fs.appendFile("log.txt", logTxt, function (err) {
+        if (err) throw err;
+    });
 }
 
 function printSongInfo(data) {
@@ -210,12 +219,12 @@ function printSongInfo(data) {
     console.log("Preview Link: " + (data).tracks.items[0].preview_url);
     console.log("Artists: " + (data).tracks.items[0].artists[0].name);
     console.log("----------------------------------------------------------------------------".green.bold);
-            
-        var logTxt =
-                "\n====New Entry======" + "Album Name: " + (data).tracks.items[0].album.name + " Song Name: " + (data).tracks.items[0].name + " Artists: " + (data).tracks.items[0].artists[0].name
-                + "\nPreview Link: " + (data).tracks.items[0].preview_url + "\n====End======"
 
-        fs.appendFile("log.txt", logTxt, function (err) {
-                if (err) throw err;
-        });
+    var logTxt =
+        "\n====New Entry======" + "Album Name: " + (data).tracks.items[0].album.name + " Song Name: " + (data).tracks.items[0].name + " Artists: " + (data).tracks.items[0].artists[0].name
+        + "\nPreview Link: " + (data).tracks.items[0].preview_url + "\n====End======"
+
+    fs.appendFile("log.txt", logTxt, function (err) {
+        if (err) throw err;
+    });
 }
